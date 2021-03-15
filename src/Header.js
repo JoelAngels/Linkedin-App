09ce6263b2bfe339442927a550fb0898abb3a8f1
JoelAngels <joelbaraka4325@gmail.com>
2021-03-15 21:17:23 +0300

    Added Material U-i dependencies

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,19 @@
 import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
 import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
-      <h1>Building our header</h1>
-      <h1>Building our header</h1>
+      <div className="header-left">
+        <img src="" alt=" " />
+
+        <div className="header-search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header-right"></div>
     </div>
   );
 }
