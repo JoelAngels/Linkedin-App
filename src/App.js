5792b03c5a01117409c3bc0573580481f9bcc85d
JoelAngels <joelbaraka4325@gmail.com>
2021-03-15 19:57:13 +0300

    Building the Header Component

--- src/App.js
+++ src/App.js
@@ -5,8 +5,27 @@ function App() {
   return (
     <div className="app">
       <h1>Lets Build</h1>
+      {/* Header */}
+      {/* App Body */}
+      {/* Sidebar  */}
+      {/* Feed */}
+      {/* Widgets */}
     </div>
   );
 }
 
 export default App;
+
+/* 
+  const firebaseConfig = {
+  apiKey: "AIzaSyADXUBgA7Wk4GxjMCsSg4taIFMiHq8KgeM",
+  authDomain: "linkedin-app-ja.firebaseapp.com",
+  projectId: "linkedin-app-ja",
+  storageBucket: "linkedin-app-ja.appspot.com",
+  messagingSenderId: "859884917146",
+  appId: "1:859884917146:web:1716dcf91f44924f9c2a23"
+};
+*/
+/*
+    Remember to check out unit testing
+ */
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Building our header</h1>
+      <h1>Building our header</h1>
+    </div>
+  );
+}
+
+export default Header;
