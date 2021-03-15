514be5248c9de7b563fb40428da304d705018c8a
JoelAngels <joelbaraka4325@gmail.com>
2021-03-15 22:45:38 +0300

    Building the header option together with the header

--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,31 @@
 import React from "react";
-import SearchIcon from "@material-ui/icons/Search";
 import "./Header.css";
+import HeaderOption from "./HeaderOption";
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
 
 function Header() {
   return (
     <div className="header">
-      <div className="header-left">
-        <img src="" alt=" " />
+      <div className="header__left">
+        <img
+          src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg"
+          alt=" "
+        />
 
-        <div className="header-search">
+        <div className="header__search">
           <SearchIcon />
-          <input type="text" />
+          <input type="text" placeholder="Search" />
         </div>
       </div>
-      <div className="header-right"></div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="Jobs" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="Messaging" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="Notifications" />
+      </div>
     </div>
   );
 }
