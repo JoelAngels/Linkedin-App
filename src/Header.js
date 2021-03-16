2721ce9e79e180d22e9dcb6fa1c982f1774d67ce
JoelAngels <joelbaraka4325@gmail.com>
2021-03-16 12:23:41 +0300

    Done styling the navbar

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,9 @@ import HeaderOption from "./HeaderOption";
 import SearchIcon from "@material-ui/icons/Search";
 import HomeIcon from "@material-ui/icons/Home";
 import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 function Header() {
   return (
@@ -22,9 +25,13 @@ function Header() {
       <div className="header__right">
         <HeaderOption Icon={HomeIcon} title="Home" />
         <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
-        <HeaderOption Icon={SupervisorAccountIcon} title="Jobs" />
-        <HeaderOption Icon={SupervisorAccountIcon} title="Messaging" />
-        <HeaderOption Icon={SupervisorAccountIcon} title="Notifications" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption
+          avatar="https://media-exp1.licdn.com/dms/image/C4D03AQHIhqf-GP660w/profile-displayphoto-shrink_100_100/0/1594657922101?e=1621468800&v=beta&t=IFAZwlqIIiaXUMFR3wKHfbOD762_02a5ZqGfnut7F94"
+          title="Me"
+        />
       </div>
     </div>
   );
