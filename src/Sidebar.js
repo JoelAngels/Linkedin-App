584bee1e4d77b5385cf02333cfc72858fc491a1d
JoelAngels <joelbaraka4325@gmail.com>
2021-03-17 10:09:47 +0300

    Finished building the sidebar section

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,6 +3,13 @@ import { Avatar } from "@material-ui/core";
 import "./Sidebar.css";
 
 function Sidebar() {
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p>{topic}</p>
+    </div>
+  );
+
   return (
     <div className="sidebar">
       <div className="sidebar__top">
@@ -28,6 +35,11 @@ function Sidebar() {
       </div>
       <div className="sidebar__bottom">
         <p>Recent</p>
+        {recentItem("reactjs")}
+        {recentItem("reactdeveloper")}
+        {recentItem("softwareengineering")}
+        {recentItem("design")}
+        {recentItem("developer")}
       </div>
     </div>
   );
