2a41f86dffabf65b93b843c1b4551eaa8069b24b
JoelAngels <joelbaraka4325@gmail.com>
2021-03-18 11:07:06 +0300

    Finished styling the Login Page, now ing on audining on adding the authentication to the login

--- src/Login.js
+++ src/Login.js
@@ -2,9 +2,33 @@ import React from "react";
 import "./Login.css";
 
 function Login() {
+  const register = () => {};
+  const loginToApp = () => {};
+
   return (
     <div className="login">
-      <h1>Rendering out the Login Page which needs to be built</h1>
+      <img
+        src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png?itok=q_lR0Vks"
+        alt=""
+      />
+
+      <form>
+        <input type="text" placeholder="Full name (required if registering)" />
+        <input type="text" placeholder="Profile pic URL (optional)" />
+        <input type="Email" placeholder="Email" />
+        <input type="Password" placeholder="Password" />
+
+        <button type="submit" onClick={loginToApp}>
+          Sign In
+        </button>
+      </form>
+
+      <p>
+        Not a Member?{"  "}
+        <span className="login__register" onClick={register}>
+          Register Now
+        </span>
+      </p>
     </div>
   );
 }
