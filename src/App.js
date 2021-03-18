b1eaabe28737ba8bbbf896cb324699bd53f8c5c4
JoelAngels <joelbaraka4325@gmail.com>
2021-03-18 10:27:29 +0300

    Used the userSlice in App Js and now building the uilding the Login Page and styling it

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
+import { selectUser } from "./features/userSlice";
+import { useSelector } from "react-redux";
+import Login from "./Login";
 
 function App() {
+  //pulling the user from the data store
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
       <Header />
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-      </div>
+      {/* If there is no user, I want to render a login Page, otherwise Render out the rest of the app */}
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+        </div>
+      )}
       {/* Widgets */}
     </div>
   );
---
+++ src/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className="login">
+      <h1>Rendering out the Login Page which needs to be built</h1>
+    </div>
+  );
+}
+
+export default Login;
